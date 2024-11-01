dbe16101f41b6a05759c2741d51c0f927ac58876
Thomas Fernandez <pepitobandito123@gmail.com>
2024-11-01 01:33:39 +0100

    Update AboutHead.tsx

--- src/components/head/AboutHead.tsx
+++ src/components/head/AboutHead.tsx
@@ -3,7 +3,7 @@ import Head from '../Head';
 
 const AboutHead: React.FC = () => (
   <Head
-    title="About Botto DAO - Our Mission and Vision"
+    title="About BottoDAO"
     description="Learn about Botto, the DAO, and what we're curating."
     image="https://cms.botto.com/assets/4679facb-a29e-466c-b7f7-5fd594fc06db"
     url="https://rare.botto.com/about"
